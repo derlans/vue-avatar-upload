c43f5e890665a0e286ea30191c1071ec6e09ea9b
derlan <1372545550@qq.com>
2022-04-14 00:02:08 +0800

    feat:Props easy look

--- preview/App.vue
+++ preview/App.vue
@@ -1,34 +1,93 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
+import { NButton, NCard, NCode, NForm, NFormItem, NInput, NInputNumber, NSwitch } from 'naive-ui'
 import AvatarUpload from '../src/index'
-const show = ref(true)
 const avatar = 'https://s3.bmp.ovh/imgs/2022/04/08/420ed87efa5616db.png'
-const headers = {
-  Authorization: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTAyNTQ4NzY2NzgwMDgwMTMwIiwiaWF0IjoxNjQ5NTgwMzA3LCJleHAiOjE2NTAxODUxMDd9.bod_OuEzqZ40mwt5w7ZBWqb-bRN-_EyfzG5Pw3BPTz_QCg-l2XrNbNL6zuNjHEdDpjp6IbbJLuSo2-MgmN2gnQ',
-}
 function handelErr(err: Error) {
-  console.log(err)
+  alert(err.message)
 }
+function handelSuccess(respose: any) {
+  alert(respose)
+}
+function handelClose() {
+  alert('click close')
+}
+const key = ref(0)
+const Props = reactive({
+  avatar,
+  handelErr,
+  width: 400,
+  height: 400,
+  selectSize: 300,
+  previewSize: 200,
+  showPreview: true,
+  rotate: true,
+  fixed: false,
+})
 </script>
 
 <template>
-  <div>
-    <AvatarUpload v-show="show" :headers="headers" :avatar="avatar" url="http://kunpeng.lhk.life/auth/user/update/avatar" :fixed="true" :on-error="handelErr" @close="()=>{show=false}" />
+  <div style="display: flex;justify-content: center;align-items: center;width: 100vw;height: 100vh;">
+    <NCard style="width: 600px;margin: 20px;" title="Props">
+      <NForm label-placement="left" label-width="100">
+        <NFormItem label="width">
+          <NInputNumber v-model:value="Props.width" style="width: 500px;" />
+        </NFormItem>
+        <NFormItem label="height">
+          <NInputNumber v-model:value="Props.height" style="width: 500px;" />
+        </NFormItem>
+        <NFormItem label="selectSize">
+          <NInputNumber v-model:value="Props.selectSize" style="width: 500px;" />
+        </NFormItem>
+        <NFormItem label="previewSize">
+          <NInputNumber v-model:value="Props.previewSize" style="width: 500px;" />
+        </NFormItem>
+        <NFormItem label="Avatar">
+          <NInput v-model:value="Props.avatar" style="width: 500px;" />
+        </NFormItem>
+        <NFormItem label="onClose">
+          <NCode>
+            {{ handelClose }}
+          </NCode>
+        </NFormItem>
+        <NFormItem label="onSuccess">
+          <NCode>
+            {{ handelSuccess }}
+          </NCode>
+        </NFormItem>
+        <NFormItem label="onError">
+          <NCode>
+            {{ handelErr }}
+          </NCode>
+        </NFormItem>
+        <NFormItem label="showPreview">
+          <NSwitch v-model:value="Props.showPreview" />
+        </NFormItem>
+        <NFormItem label="rotate">
+          <NSwitch v-model:value="Props.rotate" />
+        </NFormItem>
+        <NFormItem label="fixed">
+          <NSwitch v-model:value="Props.fixed" />
+        </NFormItem>
+        <div style="display: flex;justify-content: center;">
+          <NButton type="success" @click="key++">
+            update Props
+          </NButton>
+        </div>
+      </NForm>
+    </NCard>
+    <AvatarUpload :key="key" v-bind="Props" @close="handelClose" @error="handelErr" @success="handelSuccess" />
   </div>
-  <div>1111</div>
 </template>
 
 <style>
-html{
-    background-color: rgb(151, 151, 151);
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
+  background-color: #2c3e50;
 }
 * {
   margin: 0;
