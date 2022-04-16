162ad8dd4a24d8268c3b41b17cfa5da87d9cc7de
derlan <1372545550@qq.com>
2022-04-16 20:39:04 +0800

    feat:en description of props

--- src/index.vue
+++ src/index.vue
@@ -83,92 +83,95 @@ interface I18 {
 }
 interface AvatarUploadProps {
   /**
-   * @description 初始图像src
+   * @description 初始图像src init img src
    */
   avatar?: string
   /**
-   * @description 图片上传地址
+   * @description 图片上传地址 upload url
    */
   url?: string
   /**
-   * @description 图片上传字段名
+   * @description 图片上传字段名 upload field name
    */
   field?: string
   /**
-   * @description 图片上传格式
+   * @description 图片上传格式 upload file type
    */
   format?: string
   /**
-   * @description 上传携带请求头
+   * @description 上传携带请求头 http request headers
    */
   headers?: Record<string, string>
   /**
-   * @description 上传携带其他数据
+   * @description 上传携带其他数据 http request data
    */
   data?: Record<string, string>
   /**
-   * @description 初始宽度
+   * @description 图片框宽度  img box width
    */
   width?: number
   /**
-   * @description 初始长度
+   * @description 图片框长度  img box height
    */
   height?: number
   /**
-   * @description  选择框初始大小
+   * @description  选择框初始大小 init select box size
    */
   selectSize?: number
   /**
-   * @description 是否携带cookie
+   * @description 是否跨域携带携带cookie cross domain with cookie
    */
   withCredentials?: boolean
   /**
-   * @description 上传方法
+   * @description 上传方法 upload method
    */
   method?: 'POST' | 'GET'
   /**
-   * @description 接受的文件类型
+   * @description 接受的文件类型 accept file type
    */
   accept?: string
   /**
-   * @description 是否可以旋转
+   * @description 是否可以旋转 can rotate
    */
   rotate?: boolean
   /**
-   * @description 是否fixed
+   * @description 是否fixed  is fixed
    */
   fixed?: boolean
   /**
-   * @description 是否展示预览
+   * @description 是否展示预览 is show preview
    */
   showPreview?: boolean
   /**
-   * @description 预览框大小
+   * @description 预览框大小 preview box size
    */
   previewSize?: number
   /**
-   * @description 自定义文字
+   * @description 自定义文字 i18n
    */
   i18?: I18
+  /**
+   * @description 语言 language
+   */
   lang?: 'zh-CN' | 'zh-TW' | 'en'
   /**
-   * @description 自定义上传
+   * @description 自定义上传  custom upload
    */
   onCustomRequest?: (file: File) => void
   /**
-   * @description 上传前钩子 返回false可以阻止上传
+   * @description 上传前钩子 返回false可以阻止上传 upload before callback if return false can prevent upload
    */
   onBefoureUpload?: (file: File) => boolean | Promise<boolean>
   /**
-   * @description 上传成功钩子
+   * @description 上传成功钩子 upload success callback
    */
   onSuccess?: (respose: any, file: File) => void
   /**
-   * @description 上传失败钩子
+   * @description 上传失败钩子 upload fail callback
    */
   onError?: (err: Error, file: File) => void
   /**
-   * @description 点击关闭按钮
+   * @description 点击关闭按钮 click close button
    */
   onClose?: () => void
 }
